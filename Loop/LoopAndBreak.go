82faed540c0dc7745fdc920c71c8f52277a79b0a
SirawichDev <sirawit0676@gmail.com>
2018-01-24 21:20:23 +0700

    add 2 if statement

--- Loop/LoopAndBreak.go
+++ Loop/LoopAndBreak.go
@@ -4,11 +4,17 @@ import "fmt"
 
 func main() {
 	i := 0
+	j := 0
 	for {
-		fmt.Println(i)
+		fmt.Println("x = ", i)
+		fmt.Println("y = ", j)
 		if i >= 10 {
 			break
 		}
+		if j >= 8 {
+			break
+		}
 		i++
+		j++
 	}
 }
