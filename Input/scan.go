a6d5ce5bb30e315237b540bc885b46bd6672622d
SirawichDev <sirawit0676@gmail.com>
2018-01-24 22:28:04 +0700

    input name and age

--- Input/scan.go
+++ Input/scan.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var age string
+	var age int
+	var name string
+	fmt.Println("What's Your name ?")
+	fmt.Scan(&name)
+	fmt.Println("Hello! ", name)
 	fmt.Println("How Old Are You?")
 	fmt.Scan(&age)
-	fmt.Println("WoW You're -> ", age)
+	fmt.Println("Wow", name, " You're -> ", age, "Years old")
 
 }
