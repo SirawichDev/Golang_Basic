2876e131e703d01a7a4342fd7fd5d4138e19815e
SirawichDev <sirawit0676@gmail.com>
2018-01-24 22:30:39 +0700

    update a+b

--- Input/scan.go
+++ Input/scan.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	var a int
+	var b int
 	var age int
 	var name string
 	fmt.Println("What's Your name ?")
@@ -11,5 +13,9 @@ func main() {
 	fmt.Println("How Old Are You?")
 	fmt.Scan(&age)
 	fmt.Println("Wow", name, " You're -> ", age, "Years old")
-
+	fmt.Println("input number to a")
+	fmt.Scan(&a)
+	fmt.Println("input number to b")
+	fmt.Scan(&b)
+	fmt.Println("a+b is :", a+b)
 }
