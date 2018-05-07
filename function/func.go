43fa76a3bfa03700df50ea8717a71f1022c458d6
SirawichDev <sirawit0676@gmail.com>
2018-05-07 23:28:01 +0700

    xx

--- function/func.go
+++ function/func.go
@@ -3,45 +3,46 @@ package main
 import "fmt"
 
 //Struct
-type showFunc struct{
-            Mr string
-            name string
+type showFunc struct {
+	Mr   string
+	name string
 }
-type nomoney struct{
-          showFunc
-          Nomoney bool
+type nomoney struct {
+	showFunc
+	Nomoney bool
 }
-type ExFriend interface{
-      proof()
+type ExFriend interface {
+	proof()
+}
 
+func say(s ExFriend) {
+	s.proof()
 }
-func say (s ExFriend){
-        s.proof()
-}
+
 //Function
-func (sh showFunc) hello(){
-      fmt.Println(sh.Mr,sh.name,"say Hello!!")
+func (sh showFunc) hello() {
+	fmt.Println(sh.Mr, sh.name, "say Hellos!!")
 }
-func (nm nomoney) proof(){
-      fmt.Println(nm.Mr,nm.name,"He's doesn't has money that",nm.Nomoney)
+func (nm nomoney) proof() {
+	fmt.Println(nm.Mr, nm.name, "He's doesn't has money that", nm.Nomoney)
 }
 
-func main(){
-show := showFunc{
-        "Mr.",
-        "Sirawich",
-}
-no_mon := nomoney{
-          showFunc{
-            "Mr",
-            "Sirawich",
-          },
-          true,
-      }
-//normal
-show.hello()
-no_mon.proof()
-fmt.Println("=================")
-//polymoprism
-say(no_mon)
+func main() {
+	show := showFunc{
+		"Mr.",
+		"Sirawich",
+	}
+	no_mon := nomoney{
+		showFunc{
+			"Mr",
+			"Sirawich",
+		},
+		true,
+	}
+	//normal
+	show.hello()
+	no_mon.proof()
+	fmt.Println("=================")
+	//polymoprism
+	say(no_mon)
 }
